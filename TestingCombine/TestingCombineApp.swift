e6930cf30728d3662a28c8d9922ff8afe9a2e0db
Preston Grisham <preston@prestongrisham.com>
2020-11-24 08:45:18 -0500

    Initial Commit

---
+++ TestingCombine/TestingCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingCombineApp.swift
+//  TestingCombine
+//
+//  Created by Preston Grisham on 11/24/20.
+//
+
+import SwiftUI
+
+@main
+struct TestingCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
