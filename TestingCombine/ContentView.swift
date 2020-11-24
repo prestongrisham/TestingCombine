c00b488d7f722a5cb17b1d8558bb1c69c09881df
Preston Grisham <preston@prestongrisham.com>
2020-11-24 08:48:53 -0500

    Moved ViewModel to its own file and cleaned up app

--- TestingCombine/ContentView.swift
+++ TestingCombine/ContentView.swift
@@ -12,62 +12,6 @@ struct User: Codable, Identifiable {
     let id: Int
 }
 
-
-// ViewModels File
-import Combine
-
-final class ViewModel: ObservableObject {
-    @Published var time = ""
-    @Published var users = [User]()
-    
-    //private var anyCancellable: AnyCancellable?
-    private var cancellables = Set<AnyCancellable>()
-    
-    let formatter: DateFormatter = {
-        let df = DateFormatter()
-        df.timeStyle = .medium
-        return df
-    }()
-    
-    init() {
-        setupPublishers()
-    }
-    
-    private func setupPublishers() {
-       setupTimerPublisher()
-        setupDataTaskPublisher()
-    }
-    
-    private func setupTimerPublisher() {
-        Timer.publish(every: 1, on: .main, in: .default)
-            .autoconnect()
-            .receive(on: RunLoop.main)
-            .sink { value in
-                self.time = self.formatter.string(from: value)
-            }
-            .store(in: &cancellables)
-    }
-    
-    private func setupDataTaskPublisher() {
-        let url = URL(string: "https://jsonplaceholder.typicode.com/users")!
-        URLSession.shared.dataTaskPublisher(for: url)
-            .tryMap { (data, response) in
-                guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
-                    throw URLError(.badServerResponse)
-                }
-                return data
-            }
-            .decode(type: [User].self, decoder: JSONDecoder())
-            .receive(on: DispatchQueue.main)
-            .sink(receiveCompletion: { _ in }) { (users) in
-                self.users = users
-            }
-            .store(in: &cancellables)
-    }
-    
-}
-
-
 struct ContentView: View {
 @StateObject var viewModel = ViewModel()
     
